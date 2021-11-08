f358ebe32a0b776966175c5599ec6ad91f543f51
Lonense <41316884+Lonense@users.noreply.github.com>
2021-11-08 21:46:36 +0800

    Update update.py

--- update.py
+++ update.py
@@ -59,58 +59,57 @@ def _file_path(*other):
 
     return os.path.join(__dirname__, *other)
 
-filename=_file_path(f'astrocal.ics')
-cal = Calendar()
-cal.add("X-WR-CALNAME", "天象日历")
-cal.add("X-WR-CALDESC", "自动抓取上海天文馆数据")
-cal.add("VERSION", "2.0")
-cal.add("METHOD", "PUBLISH")
-cal.add("CLASS", "PUBLIC")
+def main():
+    filename=_file_path(f'astrocal.ics')
+    cal = Calendar()
+    cal.add("X-WR-CALNAME", "天象日历")
+    cal.add("X-WR-CALDESC", "自动抓取上海天文馆数据")
+    cal.add("VERSION", "2.0")
+    cal.add("METHOD", "PUBLISH")
+    cal.add("CLASS", "PUBLIC")
+    cal.add_component(_create_timezone())
+    now = datetime.now(ChinaTimezone())
+    all_days=[]
+    for year in range(2021,now.year+1):
+        for month in range(1,13):
+            url=requests.get(f'https://www.sstm-sam.org.cn/sam/api/hp/aps?year={year}&month={month}').text
+            if(url=='null\n'):
+                continue
+            js=json.loads(url)
+            asph=js['result']['aps'][1]
+            for event in asph:
+                name=event['astronomicalPhenomena']
+                start=_cast_date(event['date'])
+                end=_cast_date(event['date'])
+                if(event['summary'] and event['time']):
+                    descrip=event['time']+'\n'+event['summary']
+                elif(event['summary']):
+                    descrip=event['summary']
+                else:
+                    descrip=None
+                cal.add_component(_create_event(name, start, end, descrip))
 
-cal.add_component(_create_timezone())
+    with open(filename, "wb") as f:
+        f.write(cal.to_ical())     
 
-
-    
-now = datetime.now(ChinaTimezone())
-all_days=[]
-for year in range(2021,now.year+1):
-    for month in range(1,13):
-        url=requests.get(f'https://www.sstm-sam.org.cn/sam/api/hp/aps?year={year}&month={month}').text
-        if(url=='null\n'):
-            continue
-        js=json.loads(url)
-        asph=js['result']['aps'][1]
-        for event in asph:
-            name=event['astronomicalPhenomena']
-            start=_cast_date(event['date'])
-            end=_cast_date(event['date'])
-            if(event['summary'] and event['time']):
-                descrip=event['time']+'\n'+event['summary']
-            elif(event['summary']):
-                descrip=event['summary']
-            else:
-                descrip=None
-            cal.add_component(_create_event(name, start, end, descrip))
-
-with open(filename, "wb") as f:
-    f.write(cal.to_ical())     
-
-subprocess.run(["hub", "add", filename], check=True)
-diff = subprocess.run(
-    ["hub", "diff", "--stat", "--cached", "*.ics"],
-    check=True,
-    stdout=subprocess.PIPE,
-    encoding="utf-8",
-).stdout
-if not diff:
-    print("Already up to date.")
-    return
-subprocess.run(
-        [
-            "hub",
-            "commit",
-            "-m", "update"
-        ],
+    subprocess.run(["hub", "add", filename], check=True)
+    diff = subprocess.run(
+        ["hub", "diff", "--stat", "--cached", "*.ics"],
         check=True,
-    )
-subprocess.run(["hub", "push"], check=True)
+        stdout=subprocess.PIPE,
+        encoding="utf-8",
+    ).stdout
+    if not diff:
+        print("Already up to date.")
+        return
+    subprocess.run(
+            [
+                "hub",
+                "commit",
+                "-m", "update"
+            ],
+            check=True,
+        )
+    subprocess.run(["hub", "push"], check=True)
+if __name__ == "__main__":
+    main()
